3c47fae780636885408b1b75e372797aee8b568c
Misspeak <114859679+SangSX@users.noreply.github.com>
2023-05-31 21:52:13 +0800

    N update.
    Two images.

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ function brandSwich() {
 }
 
 //bar-img 事件处理
-let urlListC = ['img/sale-43-20230505.png', 'img/sale43b.jfif'];
+let urlListC = ['img/sale-43-20230601.jpg', 'img/sale-43-20230531.jpg'];
 let domC = document.getElementById("show-c");
 function changeImgC(index, loop) {
     let menu = document.getElementById("c-menu").children;
